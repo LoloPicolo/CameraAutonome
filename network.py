d613fd4b264c5a0cd249e3330f6896a7232e5a4f
LoloPicolo <57574407+LoloPicolo@users.noreply.github.com>
2024-01-18 08:46:43 +0100

    Add files via upload

---
+++ network.py
@@ -0,0 +1,22 @@
+import network
+
+# Set up the Wi-Fi networks you want to try to connect to
+networks = [("SSID1", "PSK1"), ("SSID2", "PSK2")]
+
+# Attempt to connect to each network in order
+for ssid, psk in networks:
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(ssid, psk)
+
+    # Wait for the connection to succeed or fail
+    for _ in range(20):
+        if wlan.isconnected():
+            print("Connected to", ssid)
+            break
+        else:
+            time.sleep(0.5)
+
+    # If we successfully connected to a network, stop trying
+    if wlan.isconnected():
+        break
